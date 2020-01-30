9c923ed64f44843a479157a8176f22fa341dbb42
gurux01 <gurux@gurux.fi>
2020-01-30 17:42:43 +0200

    Closing parameter added to WaitChange.

--- Gurux.DLMS.AMI.Messages/Rest/GXHost.cs
+++ Gurux.DLMS.AMI.Messages/Rest/GXHost.cs
@@ -1,7 +1,7 @@
 ﻿//
 // --------------------------------------------------------------------------
 //  Gurux Ltd
-// 
+//
 //
 //
 // Filename:        $HeadURL$
@@ -19,14 +19,14 @@
 // This file is a part of Gurux Device Framework.
 //
 // Gurux Device Framework is Open Source software; you can redistribute it
-// and/or modify it under the terms of the GNU General Public License 
+// and/or modify it under the terms of the GNU General Public License
 // as published by the Free Software Foundation; version 2 of the License.
 // Gurux Device Framework is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of 
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 // See the GNU General Public License for more details.
 //
-// This code is licensed under the GNU General Public License v2. 
+// This code is licensed under the GNU General Public License v2.
 // Full text may be retrieved at http://www.gnu.org/licenses/gpl-2.0.txt
 //---------------------------------------------------------------------------
 using Gurux.Service.Orm;
@@ -97,9 +97,30 @@ namespace Gurux.DLMS.AMI.Messages.Rest
             }
             if (waitTime == 0)
             {
-               return  h.WaitOne();
+               return h.WaitOne();
             }
             return h.WaitOne(waitTime * 1000);
+        }
+
+        /// <summary>
+        /// Wait until state has change.
+        /// </summary>
+        /// <param name="type">Change type.</param>
+        /// <param name="h">Event handle.</param>
+        /// <param name="closing">Closing event handle.</param>
+        /// <param name="waitTime">Wait time in seconds.</param>
+        /// <returns>True if event has occurred.</returns>
+        public bool WaitChange(TargetType type, AutoResetEvent h, WaitHandle closing, int waitTime)
+        {
+            lock (changes)
+            {
+                waitList.Add(new KeyValuePair<TargetType, AutoResetEvent>(type, h));
+            }
+            if (waitTime == 0)
+            {
+                return WaitHandle.WaitAny(new WaitHandle[] { h, closing }) == 0;
+            }
+            return WaitHandle.WaitAny(new WaitHandle[] { h, closing }, waitTime) == 0;
         }
 
         public void CancelChange(AutoResetEvent h)
