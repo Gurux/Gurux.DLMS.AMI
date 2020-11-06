e52c7b481f0cdcb00ec0ad93d233592b17cdb9a3
gurux01 <gurux@gurux.fi>
2020-11-06 09:14:18 +0200

    Device can be added only once for the selected reader.

--- Gurux.DLMS.AMI.Messages/DB/GXDeviceToReader.cs
+++ Gurux.DLMS.AMI.Messages/DB/GXDeviceToReader.cs
@@ -1,7 +1,7 @@
 ﻿//
 // --------------------------------------------------------------------------
 //  Gurux Ltd
-// 
+//
 //
 //
 // Filename:        $HeadURL$
@@ -19,14 +19,14 @@
 // This file is a part of Gurux Device Framework.
 //
 // Gurux Device Framework is Open Source software; you can redistribute it
-// and/or modify it under the terms of the GNU General Public License 
+// and/or modify it under the terms of the GNU General Public License
 // as published by the Free Software Foundation; version 2 of the License.
 // Gurux Device Framework is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of 
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 // See the GNU General Public License for more details.
 //
-// This code is licensed under the GNU General Public License v2. 
+// This code is licensed under the GNU General Public License v2.
 // Full text may be retrieved at http://www.gnu.org/licenses/gpl-2.0.txt
 //---------------------------------------------------------------------------
 using Gurux.Service.Orm;
@@ -38,6 +38,7 @@ namespace Gurux.DLMS.AMI.Messages.DB
     public class GXDeviceToReader
     {
         [DataMember]
+        [Index(Unique = true)]
         [ForeignKey(typeof(GXDevice), OnDelete = ForeignKeyDelete.Cascade)]
         public UInt64 DeviceId
         {
@@ -46,6 +47,7 @@ namespace Gurux.DLMS.AMI.Messages.DB
         }
 
         [DataMember]
+        [Index(Unique = true)]
         [ForeignKey(typeof(GXReaderInfo), OnDelete = ForeignKeyDelete.Cascade)]
         public UInt64 ReaderId
         {
